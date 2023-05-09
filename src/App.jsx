e36ec506e30b9629d2f35edfc6d3a70d6a6057dd
unaivan <unaivan@gmail.com>
2023-05-09 08:53:21 +0700

    push

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState ,useEffect } from 'react';
+
+import './App.css'
+
+function App() {
+
+  const [x, setX] = useState("");
+  const [y, setY] = useState("");
+  const [z, setZ] = useState("");
+  const dot = document.getElementById("sun");
+
+  function handleMotionEvent(event) {
+    console.log("handle motion event", event);
+
+    var x = event.accelerationIncludingGravity.x * 12;
+    var y = event.accelerationIncludingGravity.y * 2;
+    var z = event.accelerationIncludingGravity.z;
+
+    setX(x);
+    setY(y);
+    setZ(z);
+
+    dot.style.transform = `translate3d(${-x}px, ${y}px, ${z}px)`;
+  }
+
+  useEffect(() => {
+    window.addEventListener("devicemotion", handleMotionEvent, true);
+  }, [x, y, z]);
+
+
+
+  return (
+    <>
+      <div id="moon">
+        <div id="sun">
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
